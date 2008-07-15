12faf810cf28d469b60cf1ded252f283275450a3
yanchangyou <yanchangyou@d0816a14-0f52-0410-8eb0-43cebb87007b>
2008-07-15 12:30:46 +0000

    Added items remotely
    E:\cyyan\开发\bird 1.0\config
    E:\cyyan\开发\bird 1.0\doc
    E:\cyyan\开发\bird 1.0\lib
    E:\cyyan\开发\bird 1.0\test
    E:\cyyan\开发\bird 1.0\.classpath
    E:\cyyan\开发\bird 1.0\.project
    E:\cyyan\开发\bird 1.0\code

---
+++ config/string_rule_config/StringRuleNameConstant.java
@@ -0,0 +1,23 @@
+package string_rule_config;
+/* Copyright(c) ISoftstone,2006 - 2008
+ * 
+ * 版权声明 v1.0
+ *
+ * 本程序(包括源代码和二进制码)的知识产权(包括但不限于著作权、专利申请权、商标权、专有技术)的所有权、使用权、
+
+ * 转让权以及收益等一切权利均属于ISoftstone所有。
+
+ * ==============================================================================================================*/
+/**
+ * Class description goes here.
+ *
+ * @author <a href="mailto:cyyan@isoftstone.com">cyyan</a>
+ * @version $Id: StringRuleNameConstant.java,v0.1 2007-12-14 下午06:31:29 cyyan Exp$
+ */
+public final class StringRuleNameConstant {
+	public final static String byte_type_string_rule = "byte_type_string_rule";
+	public final static String date_string_rule = "date_string_rule";
+	public final static String date_string_rule2 = "date_string_rule2";
+	public final static String number_string_rule = "number_string_rule";
+	public final static String common_string_rule = "common_string_rule";
+}
