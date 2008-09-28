1d81a69afa1aeb52f3c259e0775b02451ee5d98c
yanchangyou <yanchangyou@d0816a14-0f52-0410-8eb0-43cebb87007b>
2008-09-28 09:48:51 +0000

    修改测试的日期

--- trunk/test/test/TypeTest.java
+++ trunk/test/test/TypeTest.java
@@ -17,49 +17,75 @@
 
 package test;
 
-import java.util.Date;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.software.bird.common.util.CommonUtil;
+import org.software.bird.rule.BreakStringRuleException;
+import org.software.bird.som.ExcelIO;
+import org.software.bird.som.exception.InvalidExcelFileException;
+import org.software.bird.som.exception.SheetNotFoundException;
+import org.software.bird.som.exception.SheetTitleNotFoundException;
+
+
 
 /**
  * Class description goes here.
- * 
+ *   
  * @author <a href="mailto:cyyan@isoftstone.com">cyyan</a>
- * @version $Id: typeTest.java,v0.1 2008-1-16 下午07:21:22 cyyan Exp$
+ * @version $Id: TestMain.java,v0.1 2007-12-6 下午01:57:37 cyyan Exp$
  */
 public class TypeTest {
-
-	private Double number;
-
-	private Date date;
-
-	private String string;
-
-	public Date getDate() {
-		return date;
+	
+	public static void main(String[] args) {		
+		
+		String fileName = "支持的变量类型.xls";
+		
+		Class[] objClassArr 
+			= new Class[] {
+//							StringTypeTest.class,
+							NumberTypeTest.class, 
+//							DateTypeTest.class,
+						   };
+		
+		
+		test(fileName, objClassArr);
 	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Double getNumber() {
-		return number;
-	}
-
-	public void setNumber(Double num) {
-		this.number = num;
-	}
-
-	public String getString() {
-		return string;
-	}
-
-	public void setString(String string) {
-		this.string = string;
-	}
-
-	public String toString() {
-		return "(" + this.number + "," + 
-				this.date + "," + 
-				this.string + ")\n";
+	
+	
+	public static void test(String excelFileName, Class[] objClassArr ) {
+		List list = null;
+		InputStream inputStream = CommonUtil.getInputStreamBySourceName(excelFileName);
+		
+		try {
+			ExcelIO aExcelIO = new ExcelIO(inputStream);
+			for (int i=0; i<objClassArr.length; i++) {
+				list = aExcelIO.readAll(objClassArr[i]);
+				System.out.println(list);
+			}
+		} catch(InvalidExcelFileException e) {
+			System.out.println(e.getMessage());
+		} catch (SheetNotFoundException e) {
+			System.out.println(e.getMessage());			
+		} catch (BreakStringRuleException e) {
+			System.out.println(e.getCause());
+			String msg = e.getMessage();
+			msg = msg == null ? "" : msg;
+			System.out.println("\n错误信息如下：\n" + msg.replaceAll("<BR>", "\n"));
+			e.printStackTrace();
+		} catch (SheetTitleNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		finally {		
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
