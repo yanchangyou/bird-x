970cb3e8cb4a68263ba60bfc227ff7b3461cb41a
yanchangyou <yanchangyou@d0816a14-0f52-0410-8eb0-43cebb87007b>
2008-09-28 09:47:09 +0000

    修改测试的日期

--- trunk/test/test/DateTypeTest.java
+++ trunk/test/test/DateTypeTest.java
@@ -1,19 +1,19 @@
 /* ====================================================================
-   Licensed to the Apache Software Foundation (ASF) under one or more
-   contributor license agreements.  See the NOTICE file distributed with
-   this work for additional information regarding copyright ownership.
-   The ASF licenses this file to You under the Apache License, Version 2.0
-   (the "License"); you may not use this file except in compliance with
-   the License.  You may obtain a copy of the License at
+ Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-==================================================================== */
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ==================================================================== */
 
 package test;
 
@@ -25,36 +25,68 @@ package test;
  */
 public class DateTypeTest {
 
-	private java.util.Date date;
+	private java.util.Date dateText;
 
-	private java.sql.Date sqlDate;
+	private java.sql.Date sqlDateText;
 
-	private java.sql.Timestamp timestamp;
+	private java.sql.Timestamp timestampText;
 
-	public java.util.Date getDate() {
-		return date;
+	private java.util.Date dateDate;
+
+	private java.sql.Date sqlDateDate;
+
+	private java.sql.Timestamp timestampDate;
+
+	public java.util.Date getDateDate() {
+		return dateDate;
 	}
 
-	public void setDate(java.util.Date date) {
-		this.date = date;
+	public void setDateDate(java.util.Date dateDate) {
+		this.dateDate = dateDate;
 	}
 
-	public java.sql.Date getSqlDate() {
-		return sqlDate;
+	public java.util.Date getDateText() {
+		return dateText;
 	}
 
-	public void setSqlDate(java.sql.Date sqlDate) {
-		this.sqlDate = sqlDate;
+	public void setDateText(java.util.Date dateText) {
+		this.dateText = dateText;
 	}
 
-	public java.sql.Timestamp getTimestamp() {
-		return timestamp;
+	public java.sql.Date getSqlDateDate() {
+		return sqlDateDate;
 	}
 
-	public void setTimestamp(java.sql.Timestamp timestamp) {
-		this.timestamp = timestamp;
+	public void setSqlDateDate(java.sql.Date sqlDateDate) {
+		this.sqlDateDate = sqlDateDate;
 	}
+
+	public java.sql.Date getSqlDateText() {
+		return sqlDateText;
+	}
+
+	public void setSqlDateText(java.sql.Date sqlDateText) {
+		this.sqlDateText = sqlDateText;
+	}
+
+	public java.sql.Timestamp getTimestampDate() {
+		return timestampDate;
+	}
+
+	public void setTimestampDate(java.sql.Timestamp timestampDate) {
+		this.timestampDate = timestampDate;
+	}
+
+	public java.sql.Timestamp getTimestampText() {
+		return timestampText;
+	}
+
+	public void setTimestampText(java.sql.Timestamp timestampText) {
+		this.timestampText = timestampText;
+	}
+
 	public String toString() {
-		return date+ ", "  +sqlDate+ ", "  +timestamp+ "\n";
+		return dateText + ", " + sqlDateText + ", " + timestampText + ", "
+				+ dateDate + ", " + sqlDateDate + ", " + timestampDate + "\n";
 	}
 }
